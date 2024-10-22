a8cfa0d889febcc75f165f0b68a3d7b04fc0816e
yogeshtavethiya <yogesh.it.kumar@gmail.com>
2024-10-22 19:22:39 +0530

    started tut

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleCount = (action)=>{
+    switch(action){
+      case 'inc':
+        setCount(prevCount=>prevCount+1);
+        break;
+      case 'dec':
+        setCount(prevCount=>prevCount-1);
+        break;
+      case'reset':
+        setCount(0);
+        break;
+      default:
+        console.error('Invalid action');
+        break;
+    }
+  }
   return (
     <div className="App">
-      
+      <h1>{count}</h1> 
+      <button onClick={()=>handleCount('inc')}>Increment</button>
+      <button onClick={()=>handleCount('dec')}>Decrement</button>
+      <button onClick={()=>handleCount('reset')}>Reset</button>
     </div>
   );
 }
